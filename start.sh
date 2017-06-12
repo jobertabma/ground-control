62a612f83716cfd67ff78f9e5253f5b352da176f
Jobert Abma <jobertabma@gmail.com>
2017-06-12 12:16:28 -0700

    Start server on port 80 and 443 (when SSL certificate exists)

--- start.sh
+++ start.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+ruby app/server.rb -p 80 &
+PORT_80_PID=$!
+
+if [ -f ssl/443.pem ]
+then
+  ruby app/server.rb -p 443 -cert ssl/443.pem &
+  PORT_443_PID=$!
+fi
+
 ruby app/server.rb -p 8080 &
 PORT_8080_PID=$!
 
@@ -8,5 +17,10 @@ PORT_8443_PID=$!
 
 read -p "Press any key to quit..." -n1 -s
 
+kill $PORT_80_PID
+if [[ ! -z $PORT_443_PID ]]
+then
+  kill $PORT_443_PID
+fi
 kill $PORT_8080_PID
 kill $PORT_8443_PID
