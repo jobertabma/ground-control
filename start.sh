71b8538b2326c3dab6cae9fe7375ef753d334a3d
Jobert Abma <jobertabma@gmail.com>
2017-06-11 22:24:21 -0700

    Delete configs, restructure app to make distinction clearer

--- start.sh
+++ start.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-ruby server.rb -p 8080 &
+ruby app/server.rb -p 8080 &
 PORT_8080_PID=$!
 
-ruby server.rb -p 8443 -cert ssl/8443.pem &
+ruby app/server.rb -p 8443 -cert ssl/8443.pem &
 PORT_8443_PID=$!
 
 read -p "Press any key to quit..." -n1 -s
