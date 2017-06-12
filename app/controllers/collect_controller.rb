a85323e6a692bb44a5dcdec74bed6a37c45aedc1
Jobert Abma <jobertabma@gmail.com>
2017-06-12 10:53:48 -0700

    Implement correct URL

--- app/controllers/collect_controller.rb
+++ app/controllers/collect_controller.rb
@@ -2,7 +2,12 @@ require_relative '../controller'
 
 class CollectController < Controller
   def get
+    @url = format '%s://%s:%d/collect',
+      req.ssl? ? 'https' : 'http',
+      req.host,
+      req.port
+
     res.content_type = 'text/javascript'
-    res.body = ERB.new(IO.read('app/views/collect.js.erb')).result
+    res.body = ERB.new(IO.read('app/views/collect.js.erb')).result(binding)
   end
 end
