5c88a0c5189ec8e031a67febe317040e3519c073
Jobert Abma <jobertabma@gmail.com>
2017-06-12 12:20:06 -0700

    Create necessary files to run server

--- install.sh
+++ install.sh
@@ -2,3 +2,15 @@
 
 # install correct Ruby version
 # generate 8443 SSL certificate with CSR
+
+# create access log
+if [ ! -f logs/access_log ]
+then
+  touch logs/access_log
+fi
+
+# seed empty configuration
+if [ ! -f config.json ]
+then
+  echo '{"callback_tokens":{}}' > config.json
+fi
