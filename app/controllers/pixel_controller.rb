dce9e9d747de7a681a735ecd5bbebc2ca2792100
Jobert Abma <jobertabma@gmail.com>
2017-06-12 12:10:15 -0700

    No need for format call

--- app/controllers/pixel_controller.rb
+++ app/controllers/pixel_controller.rb
@@ -4,9 +4,7 @@ class PixelController < Controller
   def get
     extra = CONFIG['callback_tokens'][params['callback_token']] || {}
 
-    log_request \
-      format('HTML injection callback captured', @url),
-      vulnerability_data: extra
+    log_request 'HTML injection callback captured', vulnerability_data: extra
 
     res.content_type = 'image/png'
     res.body = IO.read('static/pixel.png')
