21cffafa9790af79ab329890fc3153bb4be05cf3
Jobert Abma <jobertabma@gmail.com>
2017-06-12 12:45:40 -0700

    Generate self signed SSL certificate for port 8443

--- install.sh
+++ install.sh
@@ -3,7 +3,13 @@
 # install correct Ruby version
 rvm install 2.3
 
-# generate 8443 SSL certificate with CSR
+# generate SSL certificate for port 8443
+if [ ! -f ssl/8443.pem ]
+then
+  openssl req -x509 -newkey rsa:2048 -keyout ssl/8443.pem \
+    -out ssl/8443.pem -days 365 -nodes \
+    -subj "/C=US/ST=California/L=San Francisco/O=HackerOne, Inc./OU=NOC/CN=does.not.matter"
+fi
 
 # create access log
 if [ ! -f logs/access_log ]
